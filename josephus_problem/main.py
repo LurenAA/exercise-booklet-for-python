c84785e82b84b4cb2657aa86466020e660ad3831
xgb <978601499@qq.com>
2021-07-23 23:38:08 +0800

    添加程序刚开始运行时，选择cmd或者ui显示的功能

--- josephus_problem/main.py
+++ josephus_problem/main.py
@@ -3,13 +3,33 @@ from ui.view_controller import MainWindow
 import sys
 
 from PySide6.QtWidgets import QApplication
+import click
+
+UI = "ui"
+CMD = "cmd"
+STYLE = "--style"
+COMMAND_HELP = "选择显示模式（ui或者cmd）,不可识别的输入默认为ui"
+COMMAND_PROMPT = COMMAND_HELP
 
 
-if __name__ == "__main__":
-    if len(sys.argv) > 1 and sys.argv[1] == "cmd":
-        pass
-    else:
+@click.command()
+@click.option(
+    STYLE,
+    default=UI,
+    help=COMMAND_HELP,
+    prompt=COMMAND_PROMPT
+)
+def start(style):
+    if style not in (UI, CMD):
+        style = UI
+    if style == CMD:
+        ...
+    elif style == UI:
         app = QApplication()
         widget = MainWindow()
         widget.show()
         app.exec()
+
+
+if __name__ == "__main__":
+    start()
